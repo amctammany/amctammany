858d45813ec9861d71bd796f56c5aadb76b8e3ee
Alex McTammany <apmctammany@gmail.com>
2014-07-16 01:00:50 -0400

    rebuilt

--- dist/scripts/particle-demo.js
+++ dist/scripts/particle-demo.js
@@ -1,4 +1,6 @@
 var canvas = document.getElementById('particle-canvas');
+canvas.width = 350;
+canvas.height = 350;
 var ctx = canvas.getContext('2d');
 
 ctx.fillRect(20, 30, 10, 10);
---
+++ src/particle.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var Particle = function () {
+  this.current = new Vec2(0, 0);
+  this.previous = new Vec2(0, 0);
+};
+
